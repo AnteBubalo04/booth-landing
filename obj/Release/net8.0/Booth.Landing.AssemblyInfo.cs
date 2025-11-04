ff6721e19fe0b80cf97e81fd03f8f88ee834f0c1
AnteBubalo04 <antebubalo39@gmail.com>
2025-11-04 17:27:16 +0100

    novaraaa

--- obj/Release/net8.0/Booth.Landing.AssemblyInfo.cs
+++ obj/Release/net8.0/Booth.Landing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Booth.Landing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cd77efffe726d5c2aeed624481096d536668f7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a7325348ea131a3b1248b917b78b9a1c746110d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Booth.Landing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Booth.Landing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
